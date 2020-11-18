452ff0a1bf44af073ba46b39495219297b7e0736
Javier Ribeiro <javiersrf@gmail.com>
2020-11-18 17:44:46 -0300

    nome mudado

--- pylabansys.py
+++ pylabansys.py
@@ -4,42 +4,41 @@ import os
 class Apdlpy:
 
     def __init__(self,path_apdl,path_os):
-        self.comandos = "FINISH\\n/CLEAR\\n/e\\n"
+        self.comandos = "FINISH\n/CLEAR\n/PREP7\n"
         self.apdlpath = path_apdl
         self.ospath = path_os
         self.text_path = "comandline.txt"
         self.ready = False
-        self.arquivo = open(self.text_path,"w")
     ##comandos de kpoints
     def k(self,n,x,y,z):
-        C = "K,"+str(n)+","+str(x)+","+str(y)+","+str(z)+"\\n"
+        C = "K,"+str(n)+","+str(x)+","+str(y)+","+str(z)+"\n"
         self.comandos +=C
         
 
     def kunsel(self):
-        C = "KSEL,U,,,all\\n"
+        C = "KSEL,U,,,all\n"
         self.comandos +=C
 
     def kselall(self):
-        C = "KSEL,ALL\\n"
+        C = "KSEL,ALL\n"
         self.comandos +=C
     def kgrupo(self,nomegrupo):
-        C = "CM,"+nomegrupo+",KP\\n"
+        C = "CM,"+nomegrupo+",KP\n"
         self.comandos +=C
     ##comandos de linhas
     def l(self,n,ponto1,ponto2):
-        C = "L,"+str(n)+","+str(x)+"\\n"
+        C = "L,"+str(n)+","+str(x)+"\n"
         self.comandos +=C
 
     def lunsel(self):
-        C = "LSEL,U,,,all\\n"
+        C = "LSEL,U,,,all\n"
         self.comandos +=C
     def lselall(self):
-        C = "LSEL,ALL\\n"
+        C = "LSEL,ALL\n"
         self.comandos +=C
         
     def lgrupo(self,nomegrupo):
-        C = "CM,"+nomegrupo+",Line\\n"
+        C = "CM,"+nomegrupo+",Line\n"
         self.comandos +=C
 
     ##comandos de areas
@@ -50,53 +49,53 @@ class Apdlpy:
                 C = C+","+str(line)
             else:
                 return 0
-            C = C+"\\n"
+            C = C+"\n"
             self.comandos +=C
     def area_line_grupo(self,listLines):
-        C = "AL,grupoLsection%s\\n"
+        C = "AL,grupoLsection%s\n"
         self.comandos +=C
     
     def area_unsel(self):
-        C = "ASEL,U,,,all\\n"
+        C = "ASEL,U,,,all\n"
         self.comandos +=C
         
     def sel_a_grupo(self,nomegrupo):
-        C = "CM,"+nomegrupo+",AREA\\n"
+        C = "CM,"+nomegrupo+",AREA\n"
         self.comandos +=C
     def A(self,listkpoints):
         C = "A"
         if len(listkpoints) >= 3 and len(listkpoints) <=10:
             for line in listkpoints:
-                C = C+","+str(line)
+                C = C +","+ str(line)
                
             else:
                 return 0
-            C = C+"\\n" 
+            C = C+"\n" 
             self.comandos +=C
     ##comandos de grupo
     def create_group(self,nomegrupo,tipo):
-        C = "CM,"+str(nomegrupo)+","+str(tipo)+"\\n"
+        C = "CM,"+str(nomegrupo)+","+str(tipo)+"\n"
         self.comandos +=C   
     def g_sel(self,grupo):
-        C = "CMSEL,S,"+grupo+"\\n"
+        C = "CMSEL,S,"+grupo+"\n"
         self.comandos +=C   
 
     ##comandos de esfor;os
     def force(self,no,lab,valor1):
-        C = "F, "+str(no)+", "+str(lab)+", "+str(valor1)+", , , \\n"
+        C = "F, "+str(no)+", "+str(lab)+", "+str(valor1)+", , , \n"
         self.comandos +=C 
 
 
 
     ##comandos gerais
-    def DK(self,kpoint):
-        C = "DK,"+kpoint+",All,0.0\\n"
+    def DK(self,kpoint:str):
+        C = "DK,"+str(kpoint)+",All,0.0\n"
         self.comandos +=C
     def Allsell(self):
-        C = "allsel,all"
+        C = "allsel,all\n"
         self.comandos +=C
     def exportIGE(self,name):
-        C = "IGESOUT,"+name+",iges,,0\\n"
+        C = "IGESOUT,"+name+",iges,,0\n"
         self.comandos +=C
     def save_db(self,nome):
         C = "SAVE,"+nome+",db,,all"
--- teste.py
+++ teste.py
@@ -1,6 +1,7 @@
-import main as mn
+import pylabansys as pansys
 
-mpdl = mn.Apdlpy("ola","mundo")
+mpdl = pansys.Apdlpy("ola","mundo")
 linha = [1,2,3,4,5]
 mpdl.A(linha)
 mpdl.create_group("oa","line")
+mpdl.save_db("novo");
